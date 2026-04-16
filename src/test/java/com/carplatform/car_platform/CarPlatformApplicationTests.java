4e9fe17d1617a95facce7fd31490017adb904f03
it25102440 <it25102440@my.sliit.lk>
2026-04-16 20:16:58 +0530

    Initial project setup

---
+++ src/test/java/com/carplatform/car_platform/CarPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carplatform.car_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
